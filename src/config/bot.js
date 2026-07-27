52b96d517df84dcfa2d2aafecc49ddc5af5a009e
Ayuuuu1212121 <oxfordliner78@gmail.com>
2026-07-27 07:03:46 -0700

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -23,7 +23,7 @@ export const botConfig = {
     // 5 = Competing
     activities: [
       {
-        name: "Ayuu made me I Should surve him !", // required by Discord API, not shown in the client
+        name: "Ayuu made me I must surve him !", // required by Discord API, not shown in the client
         state: "Ayuu feeds me flowers -_- <3",     // this is what people actually see
         type: 0,               // Custom
       },
